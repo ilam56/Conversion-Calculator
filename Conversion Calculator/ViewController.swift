d2a25a3b83f2d216d0464bfde4b071d9e4879ed7
Ian Anderson <ilam56@mail.missouri.edu>
2019-11-01 11:27:51 -0500

    First commit

---
+++ Conversion Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Conversion Calculator
+//
+//  Created by Ian Anderson on 11/1/19.
+//  Copyright © 2019 Ian Anderson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
